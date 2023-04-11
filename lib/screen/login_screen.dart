551fcd51cb585bd54ba0fc6b9a578166c5ec6e98
sh-sha1248 <sia1248@kakao.com>
2023-04-11 22:44:43 +0900

    login & sign_up screen

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:every_parking/screen/sign_up_screen.dart';
+
+import 'main_screen.dart';
+
+//로그 인 후에는 앱 켤떄 안보이게 하기
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  @override
+  State<LoginScreen> createState() => _LogInState();
+}
+
+class _LogInState extends State<LoginScreen> {
+  var id = "";
+  var pass = "";
+  var testId = "id";
+  var testPass = "pass";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(0xff, 0x49, 0x7a, 0xa6),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.95,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 8), // changes position of shadow
+                        ),
+                      ]),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                        child: Text(
+                          "로그인",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Form(
+                        child: Theme(
+                          data: ThemeData(
+                            primaryColor:
+                                Color.fromARGB(0xff, 0x49, 0x7a, 0xa6),
+                            inputDecorationTheme: const InputDecorationTheme(
+                                labelStyle: TextStyle(
+                                    color:
+                                        Color.fromARGB(0xff, 0x49, 0x7a, 0xa6),
+                                    fontSize: 15.0)),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.all(40.0),
+                            child: Column(
+                              children: [
+                                TextField(
+                                  onChanged: (text) {
+                                    setState(() {
+                                      id = text;
+                                    });
+                                  },
+                                  //아이디가 입력되는 칸
+                                  decoration: InputDecoration(labelText: 'ID'),
+                                  keyboardType: TextInputType.emailAddress,
+                                ),
+                                TextField(
+                                  onChanged: (text) {
+                                    setState(() {
+                                      pass = text;
+                                    });
+                                  },
+                                  //비밀번호가 입력되는 칸
+                                  decoration:
+                                      InputDecoration(labelText: 'Password'),
+                                  keyboardType: TextInputType.text,
+                                  obscureText: true,
+                                ),
+                                const SizedBox(
+                                  height: 40.0,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ButtonTheme(
+                                        // 로그인 버튼
+                                        minWidth: 100.0,
+                                        height: 50.0,
+                                        child: ElevatedButton(
+                                            style: ButtonStyle(
+                                                textStyle:
+                                                    MaterialStateProperty.all(
+                                                        const TextStyle(
+                                                            fontSize: 14,
+                                                            color:
+                                                                Colors.white)),
+                                                backgroundColor:
+                                                    MaterialStateProperty.all(
+                                                        Color.fromARGB(0xff,
+                                                            0x49, 0x7a, 0xa6))),
+                                            child: const Text("Log in"),
+                                            onPressed: () {
+                                              //로그인 버튼을 눌렀을 때 작동될 코드
+                                              //정보가 맞다면
+                                              //홈 화면으로 이동
+                                              //정보가 틀리다면
+                                              //틀렸다고 팝업 띄우기
+                                              if (id == testId ||
+                                                  pass == testPass) {
+                                                Navigator.pushReplacement(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (BuildContext
+                                                                context) =>
+                                                            MainScreen()));
+                                              } else {
+                                                showDialog(
+                                                    context: context,
+                                                    barrierDismissible:
+                                                        true, // 바깥 영역 터치시 닫을지 여부
+                                                    builder:
+                                                        (BuildContext context) {
+                                                      return AlertDialog(
+                                                        content: const Text(
+                                                            "아이디와 비밀번호를 입력해 주세요"),
+                                                        insetPadding:
+                                                            const EdgeInsets
+                                                                    .fromLTRB(
+                                                                0, 80, 0, 80),
+                                                        actions: [
+                                                          TextButton(
+                                                            child: const Text(
+                                                                '확인'),
+                                                            onPressed: () {
+                                                              Navigator.of(
+                                                                      context)
+                                                                  .pop();
+                                                            },
+                                                          ),
+                                                        ],
+                                                      );
+                                                    });
+                                              }
+                                            })),
+                                    const SizedBox(
+                                      width: 15,
+                                    ),
+                                    ButtonTheme(
+                                        // 회원가입 버튼
+                                        minWidth: 100.0,
+                                        height: 50.0,
+                                        child: ElevatedButton(
+                                            style: ButtonStyle(
+                                                textStyle:
+                                                    MaterialStateProperty.all(
+                                                        const TextStyle(
+                                                            fontSize: 14,
+                                                            color:
+                                                                Colors.white)),
+                                                backgroundColor:
+                                                    MaterialStateProperty.all(
+                                                        Color.fromARGB(0xff,
+                                                            0x49, 0x7a, 0xa6))),
+                                            child: const Text("Sign up"),
+                                            onPressed: () {
+                                              //회원가입 화면으로 넘어가기
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        const SignUpUserScreen()),
+                                              );
+                                            })),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/sign_up_screen.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+
+//회원가입 화면
+//로그 인 후에는 앱 켤떄 안보이게 하기
+class SignUpUserScreen extends StatefulWidget {
+  const SignUpUserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpUserScreen> createState() => _SignUpUserScreen();
+}
+
+class _SignUpUserScreen extends State<SignUpUserScreen> {
+  var id = "";
+  var pass = "";
+  var s_number;
+  var u_name;
+  var isChecked = [false, false, false, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(0xff, 0x49, 0x7a, 0xa6),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.95,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 8), // changes position of shadow
+                        ),
+                      ]),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                        child: Text(
+                          "회원가입",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Form(
+                        child: Theme(
+                          data: ThemeData(
+                            primaryColor:
+                                Color.fromARGB(0xff, 0x49, 0x7a, 0xa6),
+                            inputDecorationTheme: const InputDecorationTheme(
+                                labelStyle: TextStyle(
+                                    color:
+                                        Color.fromARGB(0xff, 0x49, 0x7a, 0xa6),
+                                    fontSize: 15.0)),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Column(
+                              children: [
+                                TextField(
+                                  onChanged: (text) {
+                                    setState(() {
+                                      id = text;
+                                    });
+                                  },
+                                  //아이디가 입력되는 칸
+                                  decoration: InputDecoration(labelText: '이메일'),
+                                  keyboardType: TextInputType.emailAddress,
+                                ),
+                                TextField(
+                                  onChanged: (text) {
+                                    setState(() {
+                                      pass = text;
+                                    });
+                                  },
+                                  //비밀번호가 입력되는 칸
+                                  decoration:
+                                      InputDecoration(labelText: '비밀번호'),
+                                  keyboardType: TextInputType.text,
+                                  obscureText: true,
+                                ),
+                                TextField(
+                                  onChanged: (text) {
+                                    setState(() {
+                                      s_number = text;
+                                    });
+                                  },
+                                  //학번 입력되는 칸
+                                  decoration: InputDecoration(labelText: '학번'),
+                                  keyboardType: TextInputType.number,
+                                ),
+                                TextField(
+                                  onChanged: (text) {
+                                    setState(() {
+                                      u_name = text;
+                                    });
+                                  },
+                                  //이름 입력되는 칸
+                                  decoration: InputDecoration(labelText: '이름'),
+                                  keyboardType: TextInputType.text,
+                                ),
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                        value: isChecked[0],
+                                        onChanged: (value) {
+                                          setState(() {
+                                            isChecked[0] = value!;
+                                          });
+                                        }),
+                                    Text("이용약관"),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                        value: isChecked[1],
+                                        onChanged: (value) {
+                                          setState(() {
+                                            isChecked[1] = value!;
+                                          });
+                                        }),
+                                    Text("이용약관"),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                        value: isChecked[2],
+                                        onChanged: (value) {
+                                          setState(() {
+                                            isChecked[2] = value!;
+                                          });
+                                        }),
+                                    Text("이용약관"),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                        value: isChecked[3],
+                                        onChanged: (value) {
+                                          setState(() {
+                                            isChecked[3] = value!;
+                                          });
+                                        }),
+                                    Text("이용약관"),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ButtonTheme(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(50)),
+                                        // 로그인 버튼
+                                        minWidth: 100.0,
+                                        height: 50.0,
+                                        child: ElevatedButton(
+                                            style: ButtonStyle(
+                                                textStyle:
+                                                    MaterialStateProperty.all(
+                                                        const TextStyle(
+                                                            fontSize: 14,
+                                                            color:
+                                                                Colors.white)),
+                                                backgroundColor:
+                                                    MaterialStateProperty.all(
+                                                        Color.fromARGB(0xff,
+                                                            0x49, 0x7a, 0xa6))),
+                                            child: const Text("가입"),
+                                            onPressed: () {})),
+                                    const SizedBox(
+                                      width: 15,
+                                    ),
+                                    ButtonTheme(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(50), //모서리
+                                            side: BorderSide(
+                                                color: Color.fromARGB(
+                                                    0xff, 0x49, 0x7a, 0xa6))),
+                                        // 뒤로 버튼
+                                        minWidth: 100.0,
+                                        height: 50.0,
+                                        child: ElevatedButton(
+                                            style: ButtonStyle(
+                                                textStyle: MaterialStateProperty
+                                                    .all(const TextStyle(
+                                                        fontSize: 14,
+                                                        color: Colors.black12)),
+                                                backgroundColor:
+                                                    MaterialStateProperty.all(
+                                                        Colors.white)),
+                                            child: const Text("뒤로"),
+                                            onPressed: () {
+                                              //회원가입 화면으로 넘어가기
+                                              Navigator.pop(context);
+                                            })),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
