8026ba8e3f5b8c2f04a6f75a3f334abc425e31eb
sh-sha1248 <sia1248@kakao.com>
2023-05-20 02:56:03 +0900

    로그인 정보 로컬에 저장하기

---
+++ lib/Model/parkingMapStatue.dart
@@ -0,0 +1,28 @@
+/* 주차장 맵 구성하는데 쓰이는 것 */
+import 'dart:convert';
+import 'package:every_parking/Model/parkingarea.dart';
+
+class parkingMapStatues {
+  String name;
+  int total;
+  int used;
+  List<ParkingArea> parkingInfoList;
+
+  parkingMapStatues(
+      {required this.name,
+      required this.total,
+      required this.used,
+      required this.parkingInfoList});
+
+  factory parkingMapStatues.fromJson(Map<String, dynamic> json) {
+    List<ParkingArea> parkingInfoList = List<ParkingArea>.from(
+        json['parkingInfoList'].map((data) => ParkingArea.fromJson(data)));
+
+    return parkingMapStatues(
+      name: json['name'],
+      total: json['total'],
+      used: json['used'],
+      parkingInfoList: parkingInfoList,
+    );
+  }
+}
